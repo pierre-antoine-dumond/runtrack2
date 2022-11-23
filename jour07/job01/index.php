3fd53fda0370d3b52cd54af65e9c5988672e3928
pierre-antoine-dumond <pierre-antoine.dumond@laplateforme.io>
2022-11-23 13:23:46 +0100

    Jour07 Job01

---
+++ jour07/job01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+        <label>Nom :</label>
+        <input type="text" name="Nom">
+        <!-- <input type="text" name="Prénom"> -->
+        <input type="submit" value="Confirmer">
+    </form>
+</body>
+</html>
+
+
+<?php
+    function hello() {
+        echo '<i>Hello LaPlateforme!</i>';
+    }
+    hello();
+?>
