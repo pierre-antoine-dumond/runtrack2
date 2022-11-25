7f4efedaff40da7d5fb443363692ebb0e2bdbee2
pierre-antoine-dumond <pierre-antoine.dumond@laplateforme.io>
2022-11-25 21:11:17 +0100

    Jour10-Job01bis

--- jour10/job01/index.php
+++ jour10/job01/index.php
@@ -7,7 +7,7 @@ $db = new mysqli('localhost', $user, $pass, $db);
 $sqlQuery = 'SELECT * FROM étudiants';
 $result = mysqli_query($db, $sqlQuery) or die ("Bad Query");
 echo "<table border='1'>";
-echo "<tr><td>id</td><td>nom</td><td>naissance</td></tr>\n";
+echo "<tr><td>id</td><td>nom</td><td>naissance</td><td>sexe</td><td>email</td></tr>\n";
 while($row=mysqli_fetch_assoc($result)){
     echo "<tr><td>{$row['id']}</td><td>{$row['nom']}</td><td>{$row['naissance']}</td><td>{$row['sexe']}</td><td>{$row['email']}</td></tr>\n";
 }
